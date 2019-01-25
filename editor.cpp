7313a1ae656a60f9a937e034cfd53d1a318d8212
lbt314159 <lbtdata314@gmail.com>
2019-01-25 17:13:17 +0800

    File clean

--- editor.cpp
+++
@@ -1,9 +0,0 @@
-#include "editor.h"
-#include <Qsci/qsciscintilla.h>
-#include <Qsci/qscilexercpp.h>
-#include <Qsci/qsciapis.h>
-CodeEditor::CodeEditor(){
-}
-CodeEditor::~CodeEditor(){
-    delete textEdit;
-}
